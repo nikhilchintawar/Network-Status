8b6b440c971ca5d35b443dd355a6dd2225485f5d
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-10-17 16:56:30 +0530

    Add react-icons and show network status

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import { useEffect, useState } from "react";
+import { HiStatusOnline, HiStatusOffline } from "react-icons/hi";
 
 export interface NetworkState {
   since?: Date;
@@ -84,24 +83,20 @@ function useNetwork(): NetworkState {
 }
 
 function App() {
-  const state = useNetwork();
+  const { online } = useNetwork();
 
-  console.log(state);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {online ? (
+          <h1>
+            You are online <HiStatusOnline />
+          </h1>
+        ) : (
+          <h1>
+            You are offline <HiStatusOffline />
+          </h1>
+        )}
       </header>
     </div>
   );
